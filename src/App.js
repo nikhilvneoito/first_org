e7623fff4a6a29e36c78aa3a492ce36acc7c8465
Nikhil Vikraman <nikhil.v@neoito.com>
2023-02-06 15:21:22 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import First_org from './first_org'
+export default function App() {
+  return (
+    <div>
+      <First_org/>
+    </div>
+  )
+}
---
+++ src/first_org.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const First_org = () => {
+  return (
+   <p>Hello from first_org </p>
+  )
+}
+export default First_org
